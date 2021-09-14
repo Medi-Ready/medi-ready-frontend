b5e2c23cffe8d96fdd2164d5fec101db8c99e5a2
yerim jeon <wjsdpfla@gmail.com>
2021-09-14 13:57:19 +0900

    Style: seperate components in prescription

---
+++ src/components/Prescription/InputForm.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, useContext } from "react";
+import FlexBox from "../Shared/FlexBox";
+
+import Button from "../Shared/Button";
+import styled from "styled-components";
+import TextArea from "../Shared/TextArea";
+import TextInput from "../Shared/TextInput";
+
+import { ModalContext } from "../../contexts/ModalContext";
+import ConfirmModal from "../Shared/Modal/ConfirmModal";
+
+const InputForm = ({ isSubmit, setIsSubmit, setFormData }) => {
+  const { handleModal } = useContext(ModalContext);
+
+  const [duration, setDuration] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    setDuration("");
+    setDescription("");
+  }, [isSubmit]);
+
+  const handleChange = (event, setState) => {
+    setState(event.target.value);
+  };
+
+  const setForm = () => {
+    setFormData({ duration, description });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    handleModal(<ConfirmModal message="처방하시겠습니까?" setIsSubmit={setIsSubmit} setForm={setForm} />, "ConfirmModal");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FlexBox>
+        <div>
+          <TextArea
+            name="description"
+            onChange={(event) => handleChange(event, setDescription)}
+            value={description}>
+          </TextArea>
+        </div>
+        <InputButtonBox>
+          <TextInput
+            width="60px"
+            name="duration"
+            label="duration"
+            value={duration}
+            onChange={(event) => handleChange(event, setDuration)}
+          />
+          <span>일치</span>
+          <Button type="submit">처방</Button>
+        </InputButtonBox>
+      </FlexBox>
+    </form>
+  );
+};
+
+const InputButtonBox = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  padding-left: 10px;
+
+  > * {
+    margin-left: 20px;
+  }
+`;
+
+export default InputForm;
---
+++ src/components/Prescription/UserInfo.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+import styled from "styled-components";
+import Badge from "../Shared/Badge";
+
+const UserInfo = ({ selectedUser, isSubmit }) => {
+  const { name, picture } = selectedUser;
+
+  return (
+    <Wrapper>
+      {name && (
+        <>
+          <span>
+            <img src={picture} alt={name} />
+          </span>
+          <b>{name}</b>
+          <Badge color="green">Treating</Badge>
+        </>
+      )}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  position: relative;
+  padding-bottom: 20px;
+
+  b {
+    display: inline-block;
+    margin-left: 12px;
+    font-size: 18px;
+    font-weight: 600;
+  }
+
+  span {
+    display: inline-block;
+    font-size: 12px;
+
+    img {
+        display: inline-block;
+        width: 40px;
+        object-fit: cover;
+        border-radius: 50%;
+        vertical-align: middle;
+      }
+  }
+
+  em {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+`;
+
+export default UserInfo;
