5c6f42f1252b5d9ef45899cefdd7c1c122a33d2a
yerim jeon <wjsdpfla@gmail.com>
2021-09-15 14:50:29 +0900

    Feat: add modal type

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -3,16 +3,18 @@ import { useState } from "react";
 const useModal = () => {
   const [isModalOpened, setIsModalOpened] = useState(false);
   const [modalContent, setModalContent] = useState(null);
+  const [modalType, setModalType] = useState("");
 
   const handleModal = (content) => {
     setIsModalOpened(!isModalOpened);
 
     if (content) {
       setModalContent(content);
+      setModalType(content.type.name);
     }
   };
 
-  return { isModalOpened, handleModal, modalContent };
+  return { isModalOpened, handleModal, modalContent, modalType };
 };
 
 export default useModal;
